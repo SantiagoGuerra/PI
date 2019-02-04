40ff3e43537efa7ca5a35e5a7bf18838217b48bf
Santiago Guerra <guerrasantiago19@gmail.com>
2019-02-03 21:09:37 -0500

    Se ha creado el componente 'Text'

---
+++ src/components/Text.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledText = styled.p`
+  color: ${props => props.theme.darkColor};
+  font-family: ${props => props.theme.fontFamily};
+  font-size: ${props => props.theme.largeFontSize};
+
+  @media screen and (min-width: 760px) {
+    font-size: ${props => props.theme.extraLargeFontSize}
+  }
+
+  ${props => {
+    if(props.large) {
+      return `
+        color: ${props.theme.darkColor};
+        font-family: ${props.theme.fontFamily};
+        font-size: ${props.theme.extraLargeFontSize};
+
+        @media screen and (min-width: 760px) {
+          font-size: ${props.theme.superLargeFontSize}
+        }
+      `
+    }
+  }}
+` 
+
+const Text = ({large, children}) => (
+  <StyledText large={large}>
+    {children}
+  </StyledText>
+)
+
+
+export default Text
--- src/stories/index.js
+++ src/stories/index.js
@@ -8,10 +8,26 @@ import theme from '../styled.theme'
 import {ThemeProvider} from 'styled-components'
 
 import Logo from '../components/Logo'
+import Text from '../components/Text'
+
 
 storiesOf('Components', module)
   .add('Logo', () => (
     <ThemeProvider theme={theme}>
       <Logo/>
+    </ThemeProvider>
+  ))
+  .add('Large Text', () => (
+    <ThemeProvider theme={theme}>
+      <Text large='true'>
+      Find interesting things about your favorite number
+      </Text>
+    </ThemeProvider>
+  ))
+  .add('Text', () => (
+    <ThemeProvider theme={theme}>
+      <Text>
+      Find interesting things about your favorite number
+      </Text>
     </ThemeProvider>
   ))
