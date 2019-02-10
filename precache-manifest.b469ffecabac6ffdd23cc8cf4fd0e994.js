d8452e620133219c07bd741976e1223855df0f23
Santiago Guerra <guerrasantiago19@gmail.com>
2019-02-09 19:24:28 -0500

    Updates

---
+++ precache-manifest.b469ffecabac6ffdd23cc8cf4fd0e994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcf4ffa6b37668dd0720",
+    "url": "/PI/static/css/main.d731cde9.chunk.css"
+  },
+  {
+    "revision": "fcf4ffa6b37668dd0720",
+    "url": "/PI/static/js/main.fcf4ffa6.chunk.js"
+  },
+  {
+    "revision": "00f1c9bbf147c86c3e9e",
+    "url": "/PI/static/js/1.00f1c9bb.chunk.js"
+  },
+  {
+    "revision": "c3fa7b5c13d607d39db2",
+    "url": "/PI/static/js/runtime~main.c3fa7b5c.js"
+  },
+  {
+    "revision": "1661a5b5638a115052397d37a47bf05e",
+    "url": "/PI/index.html"
+  }
+];
